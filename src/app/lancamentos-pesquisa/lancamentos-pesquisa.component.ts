cc0d717e2b2b227e2f98a75882011a7e3547cd02
development <brennopimenta@gmail.com>
2020-07-27 19:06:52 -0300

    erro navbar component exibindoMenu

--- src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -5,11 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './lancamentos-pesquisa.component.html',
   styleUrls: ['./lancamentos-pesquisa.component.css']
 })
-export class LancamentosPesquisaComponent implements OnInit {
+export class LancamentosPesquisaComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  lancamentos = [
+    {tipo: 'DESPESA', descricao: 'Comprade Pão', dataVencimento: '30/06/2017', dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José'},
+    {tipo: 'RECEITA', descricao: 'Venda de Software', dataVencimento: '10/06/2017', dataPagamento: '09/06/2017', valor: 80000, pessoa: 'Atacado Brasil'},
+    {tipo: 'RECEITA', descricao: 'Venda de Software', dataVencimento: '10/06/2017', dataPagamento: '09/06/2017', valor: 80000, pessoa: 'Atacado Brasil'},
+    {tipo: 'RECEITA', descricao: 'Venda de Software', dataVencimento: '10/06/2017', dataPagamento: '09/06/2017', valor: 80000, pessoa: 'Atacado Brasil'},
+    {tipo: 'RECEITA', descricao: 'Venda de Software', dataVencimento: '10/06/2017', dataPagamento: '09/06/2017', valor: 80000, pessoa: 'Atacado Brasil'},
+    {tipo: 'RECEITA', descricao: 'Venda de Software', dataVencimento: '10/06/2017', dataPagamento: '09/06/2017', valor: 80000, pessoa: 'Atacado Brasil'}
+  ];
 
 }
